26951e0ea91ec7a807fab36169ec13f87eaebd34
Kokubo Atsushi <kokubo@aomori-u.ac.jp>
2012-11-12 13:08:03 +0900

    セッションをデータベースに保存するように変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-EcSite::Application.config.session_store :cookie_store, key: '_ec-site_session'
+EcSite::Application.config.session_store :active_record_store, key: '_session_id', :cookie_only => false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
