a4fb061a970f403634be0d64dc776861b6108d8a
Kokubo Atsushi <kokubo@aomori-u.ac.jp>
2012-11-12 12:17:29 +0900

    Railsプロジェクト初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcSite::Application.config.secret_token = '9d8fe32ba8b2843fcdc40bc9d51e6b4ce6fa564b4d382fb8597dd555976ccbbf37d8862aa5de89a959b0353bcce0fd9585a5ae6a76723a16d4aa6fbb8d90f259'
